46e26d71fa0bde9172fbed47aca1a1d2086bfca0
Yoon Changho <ho9938@naver.com>
2022-07-04 13:38:40 +0900

    Extra: Modify ExtraEntry

--- app/src/main/java/com/example/test3/ExtraEntry.kt
+++ app/src/main/java/com/example/test3/ExtraEntry.kt
@@ -1,14 +1,9 @@
 package com.example.test3
 
+enum class Status {
+    LAND, MINE, FLAG
+}
+
 class ExtraEntry {
-    private var m_str = "sample"
-
-    public fun setExtraEntry(m_str: String) {
-        this.m_str = m_str
-    }
-
-    public fun getExtraEntry(): String{
-        return this.m_str
-    }
-
+    private var status = Status.LAND
 }
